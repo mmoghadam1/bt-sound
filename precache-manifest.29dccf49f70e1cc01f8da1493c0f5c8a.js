4822787f2b1f8babd7559429a30a5aeec3c1251f
Maxim Moghadam <maximmoghadam@Maxims-MacBook-Pro.local>
2020-07-09 14:12:34 -0600

    Updates

---
+++ precache-manifest.29dccf49f70e1cc01f8da1493c0f5c8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6973765d50cf64f619c85a1e18fd6f",
+    "url": "/bt-sound/index.html"
+  },
+  {
+    "revision": "2e2490a90d0c1a0583f5",
+    "url": "/bt-sound/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9f736d544e27fb717ef0",
+    "url": "/bt-sound/static/css/main.7748e6b3.chunk.css"
+  },
+  {
+    "revision": "2e2490a90d0c1a0583f5",
+    "url": "/bt-sound/static/js/2.76e59933.chunk.js"
+  },
+  {
+    "revision": "51459ba9cea87b6a576e8e949a89eb86",
+    "url": "/bt-sound/static/js/2.76e59933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f736d544e27fb717ef0",
+    "url": "/bt-sound/static/js/main.fdb912e2.chunk.js"
+  },
+  {
+    "revision": "461353b0f2b9383bcd6d",
+    "url": "/bt-sound/static/js/runtime-main.e31bc573.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bt-sound/precache-manifest.29dccf49f70e1cc01f8da1493c0f5c8a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bt-sound/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
